6f19623af03d479e2168700821f34cea9ff54086
Hassan Ali <ha526779@gmail.com>
2025-08-03 23:30:19 +0500

    added commments for clarity

--- school_management_proj/students/admin.py
+++ school_management_proj/students/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Students
 
 # Register your models here.
-
+# First base model that will give base to other models
 admin.site.register(Students)

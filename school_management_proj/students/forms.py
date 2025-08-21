2db7a08d4cc96d9738660fcb261d2516d4dcd2b5
Hassan Ali <ha526779@gmail.com>
2025-08-21 22:35:26 +0500

    Changed UX and minor UI improvements

--- school_management_proj/students/forms.py
+++ school_management_proj/students/forms.py
@@ -4,8 +4,18 @@ from .models import Students
 class StudentsForm(forms.ModelForm):
     class Meta:
         model = Students
-        fields = '__all__'
+        fields = [
+             'name',
+            'father_name',
+            'roll_number',
+            'class_name',
+            'contact',
+            'fee',
+        ]
 
-
-
-
+# name = models.CharField(max_length=100)
+#     class_name = models.CharField(max_length=100)
+#     father_name = models.CharField(max_length=100)
+#     contact = models.CharField(max_length=100)
+#     roll_number = models.CharField(unique=True, max_length = 50)
+#     fee = models.IntegerField()
